3db12637da3be7d9a074393c6754ea06364f98c6
nur0m <46815432+nur0m@users.noreply.github.com>
2019-03-11 13:06:06 +0100

    Update bithostinstall.sh

--- bithostinstall.sh
+++ bithostinstall.sh
@@ -90,16 +90,16 @@ EOF
 
   systemctl daemon-reload
   sleep 3
-  systemctl start $COIN_NAME.service
-  systemctl enable $COIN_NAME.service >/dev/null 2>&1
+  #systemctl start $COIN_NAME.service
+  #systemctl enable $COIN_NAME.service >/dev/null 2>&1
 
-  if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
-    echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
-    echo -e "${GREEN}systemctl start $COIN_NAME.service"
-    echo -e "systemctl status $COIN_NAME.service"
-    echo -e "less /var/log/syslog${NC}"
-    exit 1
-  fi
+  #if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
+  #  echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
+  #  echo -e "${GREEN}systemctl start $COIN_NAME.service"
+  #  echo -e "systemctl status $COIN_NAME.service"
+  #  echo -e "less /var/log/syslog${NC}"
+  #  exit 1
+  #fi
 }
 
 
